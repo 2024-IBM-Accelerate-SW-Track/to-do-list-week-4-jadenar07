29ed2e4a9cd80898c71222ad8927ee6245a8a53d
Jaden Ritchie <ritchiejaden801@gmail.com>
2024-06-30 22:06:18 -0400

    changes

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
-import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import Axios from "axios";
 
 class AddTodo extends Component {
-  // Create a local react state of the this component with both content date property set to nothing.
+  // Create a local react state of this component with both content date property set to nothing.
   constructor() {
     super();
     this.state = {
@@ -13,33 +14,56 @@ class AddTodo extends Component {
       duedate: null
     };
   }
+
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
   // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
   // into the text field.
   handleChange = (event) => {
     this.setState({
       content: event.target.value,
-      date: Date()
+      date: new Date().toString()
     });
   };
 
   handleDateChange = (event) => {
-    let date = null
-    if(event != null){
-      date = new Date(event)
+    let date = null;
+    if (event != null) {
+      date = new Date(event);
     }
     this.setState({
       duedate: date
     });
   };
-  // The handleSubmit function collects the forms input and puts it into the react state.
-  // event.preventDefault() is called to prevents default event behavior like refreshing the browser.
+
+  // The handleSubmit function collects the form's input and puts it into the react state.
+  // event.preventDefault() is called to prevent default event behavior like refreshing the browser.
   // this.props.addTodo(this.state) passes the current state (or user input and current date/time) into the addTodo function defined
   // in the Home.js file which then adds the input into the list.
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
-      this.props.addTodo(this.state);
+      const jsonObject = {
+        id: new Date().getTime(),
+        task: this.state.content,
+        currentDate: this.state.date,
+        dueDate: this.state.duedate ? this.state.duedate.toString() : null
+      };
+
+      // Making the POST request to the addItem function in the Express application
+      Axios({
+        method: "POST",
+        url: "http://localhost:5001/add/item",
+        data: jsonObject,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(res => {
+        console.log(res.data.message);
+      }).catch(error => {
+        console.error("There was an error making the request:", error);
+      });
+
+      this.props.addTodo(jsonObject);
       this.setState({
         content: "",
         date: "",
@@ -47,6 +71,7 @@ class AddTodo extends Component {
       });
     }
   };
+
   render() {
     return (
       // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
@@ -54,7 +79,7 @@ class AddTodo extends Component {
       // 2. The return statement should include a text field input with the handleChange function from above that
       // is passed into an onChange event.
       // 3. The return should also include a button with the handleSubmit function from above that is passed into
-      // an OnClick event.
+      // an onClick event.
       // 4. The value of the text field also should reflect the local state of this component.
       <div>
         <TextField
@@ -63,13 +88,13 @@ class AddTodo extends Component {
           onChange={this.handleChange}
           value={this.state.content}
         />
-        <LocalizationProvider dateAdapter={AdapterDateFns}>         
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
           <DesktopDatePicker
-              id="new-item-date"
-              label="Due Date"
-              value={this.state.duedate}
-              onChange={this.handleDateChange}
-              renderInput={(params) => <TextField {...params} />}
+            id="new-item-date"
+            label="Due Date"
+            value={this.state.duedate}
+            onChange={this.handleDateChange}
+            renderInput={(params) => <TextField {...params} />}
           />
         </LocalizationProvider>
         <Button
